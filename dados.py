17536e4d38cfeeff57be9312babc7d89192724b0
diogogaldino <50590826+diogogaldino@users.noreply.github.com>
2019-05-13 14:17:05 -0300

    dados: completo

--- dados.py
+++ dados.py
@@ -1,18 +1,32 @@
-#Meu primeiro programa
+# Meu primeiro programa
 # diogogaldino <diogo.galdino.ifc@gmail.com>
 
-from random import randint #Importa a função de gerar números aleatórios
+from random import randint  # Importa a função de gerar números aleatórios
 
 print("Jogo dos dados: ")
 
-dado1 = randint(1, 6) #Gera um número entre 1 e 6 aleatoriamente
+dado1 = randint(1, 6)  # Gera um número entre 1 e 6 aleatoriamente
 print("Dado 1: ", dado1)
 
-dado2 = randint(1, 6) #Gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado1)
+dado2 = randint(1, 6)  # Gera um número entre 1 e 6 aleatoriamente
+print("Dado 2: ", dado2)
 
-dado3 = randint(1, 6) #Gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado1)
+dado3 = randint(1, 6)  # Gera um número entre 1 e 6 aleatoriamente
+print("Dado 3: ", dado3)
 
-dado4 = randint(1, 6) #Gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado1)
+dado4 = randint(1, 6)  # Gera um número entre 1 e 6 aleatoriamente
+print("Dado 4: ", dado4)
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print('Jogador 1: ', jogador1)
+print('Jogador 2: ', jogador2)
+
+if jogador1 > jogador2:
+    print("Jogador 1 venceu")
+else:
+    if jogador2 > jogador1:
+        print("Jogador 2 venceu!")
+    else:
+        print("Empate!")
